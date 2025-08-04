6b01031594b2614013940660183796f979af3f33
Tahasin Islam <ghostcorp39@gmail.com>
2025-08-04 17:52:37 +0600

    Update result.php

--- result.php
+++ result.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@3.3.3/dist/tailwind.min.css">
@@ -7,8 +10,6 @@
 <script src="https://cdn.jsdelivr.net/npm/vue@3.4.21/dist/vue.global.prod.js"></script>
 <!-- Add more libraries as needed by including their CDN links in your code -->
 <?php
-session_start();
-
 if (isset($_GET['fullscreen'])) {
     echo '<a href="index.php" style="font-size:15px;float:right;">'
         . '      <button style="cursor:pointer">'
@@ -22,3 +23,4 @@ if (!isset($_SESSION['code'])) {
     $code = $_SESSION['code'];
     echo $code;
 }
+
